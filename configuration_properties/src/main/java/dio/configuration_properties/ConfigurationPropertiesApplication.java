789c6e3455d70e4768015ee6a4ebee7c09c8e030
pedroanicio <pedranx@gmail.com>
2024-06-06 12:52:34 -0300

    adding config properties

---
+++ configuration_properties/src/main/java/dio/configuration_properties/ConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package dio.configuration_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationPropertiesApplication.class, args);
+	}
+
+}
---
+++ configuration_properties/src/main/java/dio/configuration_properties/app/SistemaMensagem.java
@@ -0,0 +1,21 @@
+package dio.configuration_properties.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+    @Autowired
+    private Remetente rementente;
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: " + rementente.getNome()
+                + "\nE-mail:" + rementente.getEmail() + "\nCom telefones para contato: "
+                + rementente.getTelefones());
+        System.out.println("Seu cadastro foi aprovado");
+    }
+}
