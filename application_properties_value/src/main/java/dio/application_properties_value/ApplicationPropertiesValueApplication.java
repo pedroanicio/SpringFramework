1418acaabcad2f47bf30d3a0d42749f75b22877e
pedroanicio <pedranx@gmail.com>
2024-06-06 12:34:47 -0300

    adding properties value

---
+++ application_properties_value/src/main/java/dio/application_properties_value/ApplicationPropertiesValueApplication.java
@@ -0,0 +1,13 @@
+package dio.application_properties_value;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPropertiesValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationPropertiesValueApplication.class, args);
+	}
+
+}
---
+++ application_properties_value/src/test/java/dio/application_properties_value/ApplicationPropertiesValueApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.application_properties_value;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPropertiesValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
