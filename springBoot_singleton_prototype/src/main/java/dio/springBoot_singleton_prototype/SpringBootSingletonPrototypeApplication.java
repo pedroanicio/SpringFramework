ae387b8cd29e1ed76fb366cca71752df0582c050
pedroanicio <pedranx@gmail.com>
2024-06-06 11:57:29 -0300

    adding singleton_prototype

--- springBoot_singleton_prototype/src/main/java/dio/springBoot_singleton_prototype/SpringBootSingletonPrototypeApplication.java
+++ springBoot_singleton_prototype/src/main/java/dio/springBoot_singleton_prototype/SpringBootSingletonPrototypeApplication.java
@@ -1,7 +1,10 @@
 package dio.springBoot_singleton_prototype;
 
+import dio.springBoot_singleton_prototype.app.SistemaMensagem;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSingletonPrototypeApplication {
@@ -10,4 +13,12 @@ public class SpringBootSingletonPrototypeApplication {
 		SpringApplication.run(SpringBootSingletonPrototypeApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(SistemaMensagem sitema) throws Exception{
+		return args -> {
+			sitema.enviarConfirmacaoCadastro();
+			sitema.enviarMensagemBoasVindas();
+			sitema.enviarConfirmacaoCadastro();
+		};
+	}
 }
