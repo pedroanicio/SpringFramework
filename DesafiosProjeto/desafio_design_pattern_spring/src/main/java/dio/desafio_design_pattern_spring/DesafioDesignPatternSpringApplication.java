d61686ded215afc7332d8a67810ad9bf0d41a901
pedroanicio <pedranx@gmail.com>
2024-06-13 20:41:55 -0300

    adding desafio

---
+++ DesafiosProjeto/desafio_design_pattern_spring/src/main/java/dio/desafio_design_pattern_spring/DesafioDesignPatternSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.desafio_design_pattern_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDesignPatternSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDesignPatternSpringApplication.class, args);
+	}
+
+}
---
+++ DesafiosProjeto/desafio_design_pattern_spring/src/test/java/dio/desafio_design_pattern_spring/DesafioDesignPatternSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_design_pattern_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDesignPatternSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
