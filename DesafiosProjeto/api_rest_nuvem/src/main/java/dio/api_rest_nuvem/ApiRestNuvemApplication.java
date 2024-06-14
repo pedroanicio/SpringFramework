152878b202adade702d2137877a6ca87acf4ff13
pedroanicio <pedranx@gmail.com>
2024-06-14 19:33:31 -0300

    adding new desafio

---
+++ DesafiosProjeto/api_rest_nuvem/src/main/java/dio/api_rest_nuvem/ApiRestNuvemApplication.java
@@ -0,0 +1,13 @@
+package dio.api_rest_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNuvemApplication.class, args);
+	}
+
+}
